19976bd51e89eb498047eaa843b0fa1d2c492805
Manikandan-prog <84707867+Manikandan-prog@users.noreply.github.com>
2021-10-25 19:14:00 +0530

    Update Maven_Sonar.java

--- demo02/src/main/java/demo02/Maven_Sonar.java
+++ demo02/src/main/java/demo02/Maven_Sonar.java
@@ -2,7 +2,7 @@ package demo02;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.message.LoggerNameAwareMessage;
+
 
 public class Maven_Sonar {
 
