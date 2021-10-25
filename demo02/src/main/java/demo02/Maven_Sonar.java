c48375f9c1af441d7ab7313328855064de23550f
Manikandan-prog <84707867+Manikandan-prog@users.noreply.github.com>
2021-10-25 19:08:29 +0530

    Update Maven_Sonar.java

--- demo02/src/main/java/demo02/Maven_Sonar.java
+++ demo02/src/main/java/demo02/Maven_Sonar.java
@@ -6,16 +6,12 @@ import org.apache.logging.log4j.message.LoggerNameAwareMessage;
 
 public class Maven_Sonar {
 
-//			private void aaa() {
-//				if("xx".length() > 7) {
-//					
-//				}
-//			}
+
 	private static Logger logger = LogManager.getLogger(Maven_Sonar.class);
 
 		    public static void main( String[] args )
 		    {
-		        System.out.println( "Hello World!" );
+		        
 		        
 		        logger.info("This is info");
 		        logger.error("This is error");
